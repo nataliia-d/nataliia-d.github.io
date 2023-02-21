bf9baef7063e430511e6051535a7a2f3898fbac3
nataliia-d <82103446+nataliia-d@users.noreply.github.com>
2023-02-20 20:10:33 -0500

    javascript added

---
+++ main.js
@@ -0,0 +1,10 @@
+
+console.log("java script is working!!!");
+
+let e = document.querySelector(".left");
+let fontSize = 10;
+
+// setInterval( ()=> {
+//     e.style.fontSize = fontSize + "px";
+//     fontSize++;
+// }, 100);
