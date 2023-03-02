e7e3a371bb5c98fc86f8a2baaf683c12223e99ff
nataliia-d <82103446+nataliia-d@users.noreply.github.com>
2023-03-02 18:29:05 -0500

    fix

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const expandsMore = document.querySelectorAll('[expand-more]')
+function doSomething() {
+  const expandsMore = document.querySelectorAll('[expand-more]');
 
-    function expand(){
-        const showContent = document.getElementById(this.dataset.target)
-        if (showContent.classList.contains('expand-active')){
-            this.innerHTML=this.dataset.showtext
-        }
-        else{
-            this.innerHTML=this.dataset.hidetext
-        }
-        showContent.classList.toggle('expand-active')
+  function expand() {
+    const showContent = document.getElementById(this.dataset.target);
+    if (showContent.classList.contains('expand-active')) {
+      this.innerHTML = this.dataset.showtext;
+    } else {
+      this.innerHTML = this.dataset.hidetext;
     }
+    showContent.classList.toggle('expand-active');
+  }
 
-    expandsMore.forEach(expandMore => {
-        expandMore.addEventListener('click', expand)
-    })
-})
+  expandsMore.forEach((expandMore) => {
+    expandMore.addEventListener('click', expand);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', doSomething);
