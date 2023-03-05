0322293d3695e77f190e0ba73f272dab9817193e
nataliia-d <82103446+nataliia-d@users.noreply.github.com>
2023-03-05 11:04:06 -0500

    drag but no drop

--- main.js
+++ main.js
@@ -14,6 +14,58 @@ function doSomething() {
   expandsMore.forEach((expandMore) => {
     expandMore.addEventListener('click', expand);
   });
+
+  // select the item element
+  const item = document.querySelector('.frog');
+
+  // handle the dragstart
+  function dragStart(e) {
+    console.log('drag starts...');
+    e.dataTransfer.setData('text/plain', e.target.id);
+    setTimeout(() => {
+      e.target.classList.add('hide');
+    }, 0);
+  }
+
+  // attach the dragstart event handler
+  item.addEventListener('dragstart', dragStart);
+
+  const boxes = document.querySelectorAll('.drag-slot');
+
+  function dragEnter(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+  }
+
+  function dragOver(e) {
+    e.preventDefault();
+    e.target.classList.add('drag-over');
+  }
+
+  function dragLeave(e) {
+    e.target.classList.remove('drag-over');
+  }
+
+  function drop(e) {
+    e.target.classList.remove('drag-over');
+
+    // get the draggable element
+    const id = e.dataTransfer.getData('text/plain');
+    const draggable = document.getElementById(id);
+
+    // add it to the drop target
+    e.target.appendChild(draggable);
+
+    // display the draggable element
+    draggable.classList.remove('hide');
+  }
+
+  boxes.forEach((box) => {
+    box.addEventListener('dragenter', dragEnter);
+    box.addEventListener('dragover', dragOver);
+    box.addEventListener('dragleave', dragLeave);
+    box.addEventListener('drop', drop);
+  });
 }
 
 document.addEventListener('DOMContentLoaded', doSomething);
