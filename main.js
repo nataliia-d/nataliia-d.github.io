2e9e5cb9ec0eb12d5a0c57275ab96e2d80aa8d0e
nataliia-d <82103446+nataliia-d@users.noreply.github.com>
2023-03-06 19:33:42 -0500

    gallery

---
+++ main.js
@@ -0,0 +1,19 @@
+function doSomething() {
+  const expandsMore = document.querySelectorAll('[expand-more]');
+
+  function expand() {
+    const showContent = document.getElementById(this.dataset.target);
+    if (showContent.classList.contains('expand-active')) {
+      this.innerHTML = this.dataset.showtext;
+    } else {
+      this.innerHTML = this.dataset.hidetext;
+    }
+    showContent.classList.toggle('expand-active');
+  }
+
+  expandsMore.forEach((expandMore) => {
+    expandMore.addEventListener('click', expand);
+  });
+}
+
+document.addEventListener('DOMContentLoaded', doSomething);
